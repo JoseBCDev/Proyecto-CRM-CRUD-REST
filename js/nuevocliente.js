c8fdcaf32ee2a419eca7c615d942197877aad041
xdgonex <bendezucortezjoseluis@gmail.com>
2022-10-22 17:20:38 -0500

    Validando formulario de nuevocliente.js

---
+++ js/nuevocliente.js
@@ -0,0 +1,42 @@
+
+//Funcion ifi (permite que se mantengan las variables y funciones)
+(function(){
+   
+    const formulario = document.querySelector('#formulario');
+
+    document.addEventListener('DOMContentLoaded',()=>{
+
+        formulario.addEventListener('submit',validarFormulario);
+    })
+
+
+    //Funciones
+    function validarFormulario(e)
+    {
+        e.preventDefault();
+    
+        const nombre = document.querySelector('#nombre').value;
+        const email = document.querySelector('#email').value;
+        const telefono = document.querySelector('#telefono').value;
+        const empresa = document.querySelector('#empresa').value;
+        //Creamos objeto pasandole los datos del input
+        const cliente ={
+            nombre,
+            email,
+            telefono,
+            empresa
+        }
+        //Validamos si estan vacios o no los inputs
+        if(!validarCampos(cliente))
+        {
+            console.log('Datos en blanco');
+        }
+
+            console.log('Datos completos');
+    }
+
+    function validarCampos(objeto)
+    {//Every solo Retorna TRUE OR FALSE, iteramos en el objeto
+        return Object.values(objeto).every(value => value !=='' )
+    }
+})();
